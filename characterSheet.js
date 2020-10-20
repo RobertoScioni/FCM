0f225c2a32bb6e5667cc017a646f1c768520c12e
Roberto <72069799+RobertoScioni@users.noreply.github.com>
2020-10-20 23:24:37 +0200

    create an object prototype for a character

--- characterSheet.js
+++ characterSheet.js
@@ -1,3 +1,10 @@
+const character={
+    Aspects:[];
+    Skills[];
+    Stunts:[];
+    Extra:[];
+}
+
 const printSheet=()=>{
     /*this function will return a DOM object containing a complete character sheet*/
 }

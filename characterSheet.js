fb5574c7c7cc5499703f6501b3496a1a4de7c923
Roberto <72069799+RobertoScioni@users.noreply.github.com>
2020-11-04 23:13:39 +0100

    probably obsolete

--- characterSheet.js
+++ characterSheet.js
@@ -1,9 +1,13 @@
 const character={
+    Refresh:3,
+    FatePoints:3,
     Aspects:[],
     Skills:[],
     Stunts:[],
     Extra:[]
 }
+
+const characters=[]
 
 const printSheet=()=>{
     /*this function will return a DOM object containing a complete character sheet*/

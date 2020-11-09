1b6a148f5a3a1d31ed96225d29e639725fc30ce3
Roberto <72069799+RobertoScioni@users.noreply.github.com>
2020-11-09 16:55:29 +0100

    refactored aspects rappresentation

--- characterSheet.js
+++ characterSheet.js
@@ -6,13 +6,21 @@ const initializeEnvironement = () => {
 		.then((usefuldata) => {
 			console.log("usefuldata", usefuldata)
 			characters = usefuldata
+			const deck = document.querySelector("#DECK")
+			deck.appendChild(printSheet(usefuldata[0]))
 		})
 }
 
 const printSheet = (character) => {
 	/*this function will return a DOM object containing a complete character sheet*/
-	const char = document.querySelector(".character-template").cloneNode
-	char.classlist.remove(".character-template")
-	char.querySelector("name") = character.name
+	const char = document.querySelector(".character-template").cloneNode(true)
+	char.classList.remove("character-template")
+	char.querySelector(".name").innerText = character.name
+	char.aspects.forEach((Aspect) => {
+		let aspect = document.createElement("li")
+		aspect.classList.add("list-group-item")
+		aspect.innerText = Aspect
+		char.querySelector(".aspects").appendChild(aspect)
+	})
 	return char
 }

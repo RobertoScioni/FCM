19a40b53f594f36fdc0f5ea1bae93538073425d2
Roberto <72069799+RobertoScioni@users.noreply.github.com>
2020-11-09 15:29:14 +0100

    test retrieving server data from a file

--- characterSheet.js
+++ characterSheet.js
@@ -1,4 +1,7 @@
+const characters="https://robertoscioni.github.io/FCM/character.json"
+
 const printSheet=()=>{
     /*this function will return a DOM object containing a complete character sheet*/
+    fetch(./character.json).then((characters)=>console.log(characters))
 }
 

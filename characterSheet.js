c8a5c2c5925081ac2602e2c9ebe64184c1948d6e
Roberto <72069799+RobertoScioni@users.noreply.github.com>
2020-11-09 16:36:29 +0100

    started data mangling

--- characterSheet.js
+++ characterSheet.js
@@ -1,7 +1,18 @@
-const characters="https://robertoscioni.github.io/FCM/character.json"
-
-const printSheet=()=>{
-    /*this function will return a DOM object containing a complete character sheet*/
-    fetch(./character.json).then((characters)=>console.log(characters))
+const charactersURL = "https://robertoscioni.github.io/FCM/character.json"
+let characters
+const initializeEnvironement = () => {
+	fetch(charactersURL)
+		.then((chars) => chars.json())
+		.then((usefuldata) => {
+			console.log("usefuldata", usefuldata)
+			characters = usefuldata
+		})
 }
 
+const printSheet = (character) => {
+	/*this function will return a DOM object containing a complete character sheet*/
+	const char = document.querySelector(".character-template").cloneNode
+	char.classlist.remove(".character-template")
+	char.querySelector("name") = character.name
+	return char
+}
